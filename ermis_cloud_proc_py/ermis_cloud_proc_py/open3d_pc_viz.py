a68cbe62372968b6e93ae3392e9db958f38af0d7
FranciscoMColino <francisco.m.colino@gmail.com>
2024-05-29 00:18:00 +0100

    Add color to cluster and fixed infinite points issue

--- ermis_cloud_proc_py/ermis_cloud_proc_py/open3d_pc_viz.py
+++ ermis_cloud_proc_py/ermis_cloud_proc_py/open3d_pc_viz.py
@@ -4,6 +4,7 @@ from sensor_msgs.msg import PointCloud2
 import open3d as o3d
 import sensor_msgs_py.point_cloud2 as pc2
 import numpy as np
+import struct
 
 class PointCloudSubscriber(Node):
     def __init__(self):
@@ -12,24 +13,55 @@ class PointCloudSubscriber(Node):
             PointCloud2,
             '/zed/zed_node/point_cloud/cloud_registered',
             self.pointcloud_callback,
-            2)
+            10)
         self.subscription  # prevent unused variable warning
         self.vis = o3d.visualization.Visualizer()
+        self.pcd = o3d.geometry.PointCloud()
         self.vis.create_window()
         self.first_run = True
 
     def pointcloud_callback(self, msg):
-        # Convert ROS PointCloud2 message to numpy array
-        pc2_points = pc2.read_points_numpy(msg, field_names=("x", "y", "z"), skip_nans=True)
 
-        self.vis.clear_geometries()
-        pcd = o3d.geometry.PointCloud(
-            o3d.utility.Vector3dVector(pc2_points))
-        pcd.paint_uniform_color([0.5, 0.5, 0.5])
-        self.vis.add_geometry(pcd, reset_bounding_box=self.first_run)
+        #self.vis.clear_geometries()
+
+        # Convert ROS PointCloud2 message to numpy array
+        pc2_data = pc2.read_points_numpy(msg, field_names=("x", "y", "z", 'rgb'), skip_nans=True)
+        # separate the rgb values
+        pc2_data = np.array(list(pc2_data))
+        pc2_points = pc2_data[:, :3]
+
+        # Unpack RGB values
+        pc2_colors = pc2_data[:, 3]
+        colors = np.zeros((pc2_colors.size, 3), dtype=np.float32)
+
+        for i, color in enumerate(pc2_colors):
+            packed = struct.unpack('I', struct.pack('f', color))[0]
+            r = (packed & 0x00FF0000) >> 16
+            g = (packed & 0x0000FF00) >> 8
+            b = (packed & 0x000000FF)
+            colors[i, :] = [r, g, b]
+
+        colors /= 255.0
+
+        # get indexes where points are infinite
+        inf_idx = np.isinf(pc2_points).any(axis=1)
+
+        # remove infinite points from both the point cloud and the colors
+        pc2_points = pc2_points[~inf_idx]
+        colors = colors[~inf_idx]
+
+        #print(f'Removing {inf_idx.sum()} infinite points.')
+        
+        # Update the point cloud
+        self.pcd.points = o3d.utility.Vector3dVector(pc2_points)
+        self.pcd.colors = o3d.utility.Vector3dVector(colors)
 
         if self.first_run:
+            self.vis.add_geometry(self.pcd, reset_bounding_box=True)
+            self.vis.add_geometry(o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.5), reset_bounding_box=True)
             self.first_run = False
+        else:
+            self.vis.update_geometry(self.pcd)
 
         # Clear and update the visualizer
         self.vis.poll_events()
